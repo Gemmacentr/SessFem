707132f7125203222eb4a01503f911c8763e9163
Gemma Centrone <56439561+Gemmacentr@users.noreply.github.com>
2024-05-06 14:01:08 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:  process.env.NODE_ENV === "production" ? "/SessFem/" : "/",
-  publicPath: process.env.NODE_ENV === "production" ? "/SessFem/" : "/",
+  base: "/",
   plugins: [vue()],
 });
